6a170b3ed25860391b19487af662e4465c4e3ca7
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-03-26 20:05:48 +0100

    Add basic layout for home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,31 @@
 @extends('layouts.header')
 
 @section('content')
-    <h1>Mythos</h1>
-    @foreach ($posts as $key => $post)
-        <p>{{ $post->title }}</p>
-    @endforeach
+    <div class="navbar">
+        <h1>Mythos</h1>
+        <div class="links">
+            <a href={{ route('home') }}>Home</a>
+        </div>
+    </div>
+    <div class="posts">
+        @foreach ($posts as $key => $post)
+            <div class="post">
+                <div class="title">{{ $post->title }}</div>
+                <div class="tags">
+                    @if (str_contains($post->tags, ','))
+                        {{ $data = explode(',', $post->tags) }}
+                        @foreach ($data as $key => $tag)
+                            <div class="tag">{{ $tag }}</div>
+                        @endforeach
+                    @else
+                        <div class="tag">{{ $post->tags }}</div>
+                    @endif
+                </div>
+                <div class="description">
+                    {{ $post->description }}
+                </div>
+                <div class="author">author: {{ $post->author }}</div>
+            </div>
+        @endforeach
+    </div>
 @endsection
